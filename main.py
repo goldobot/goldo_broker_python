8755eccfaf9832e0d8077e0961d73363a62b4bae
Goldorak <echelon@free.fr>
2023-08-19 23:10:42 +0200

    * Display elapsed time in stats also.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import sys
 import signal
+import time
 
 from goldo_broker.broker.broker_process import run_broker_process
 from goldo_broker.broker.zmq_codecs import nucleo_in_stat,nucleo_out_stat,nucleo_out_bad_stat,nucleo_very_bad_stat
 
+stats_t0 = 0
+
 def goldo_signal_handler(sig,frame):
+    stats_t1 = time.time()
     print (" DEBUG GOLDO : sys.exit(0)")
+    print ("elapsed time:")
+    print (stats_t1 - stats_t0)
     print ("nucleo_in_stat:")
     for k in nucleo_in_stat.keys():
         print (k, nucleo_in_stat[k])
@@ -23,5 +29,6 @@ def goldo_signal_handler(sig,frame):
     sys.exit(0)
 
 if __name__ == '__main__':
+    stats_t0 = time.time()
     signal.signal(signal.SIGINT, lambda sig,frame: goldo_signal_handler(sig,frame))
     run_broker_process()
